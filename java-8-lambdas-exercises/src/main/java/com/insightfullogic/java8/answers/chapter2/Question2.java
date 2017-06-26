df6ac46c7ffab9f547bc010ee1867637aa8cfc79
Yo <123>
2017-06-26 22:15:40 +0800

    练习

--- java-8-lambdas-exercises/src/main/java/com/insightfullogic/java8/answers/chapter2/Question2.java
+++ java-8-lambdas-exercises/src/main/java/com/insightfullogic/java8/answers/chapter2/Question2.java
@@ -1,12 +1,19 @@
 package com.insightfullogic.java8.answers.chapter2;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 import javax.swing.text.DateFormatter;
 
 import static java.lang.ThreadLocal.withInitial;
 
 public class Question2 {
 
-     public final static ThreadLocal<DateFormatter> formatter = ThreadLocal.withInitial(() -> new DateFormatter(new SimpleDateFormat("dd-MMM-yyyy")));
+     public final static ThreadLocal<DateFormatter> formatter =
+             ThreadLocal.withInitial(() -> new DateFormatter(new SimpleDateFormat("dd-MMM-yyyy")));
 
+     public static void main(String [] args){
+         System.out.println(formatter.get().getFormat().format(new Date()));
+     }
 }
